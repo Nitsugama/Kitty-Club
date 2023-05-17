074d71dd0c261692aae8906c99e858955c846784
Khan Ankha <khanankha@proton.me>
2023-05-17 15:06:28 -0300

    Criado rota para os videos

--- routes/videoPlayer.js
+++ routes/videoPlayer.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/:id', function(req, res, next) {
   const imageName = req.params.id;
-  const caminhoImagem = `/images/${imageName}.jpg`; // Substitua pela extensão correta da imagem, se necessário
+  const caminhoImagem = `/images/${imageName}.mp4`; // Substitua pela extensão correta da imagem, se necessário
   res.render('videoPlayer', { title: 'Kitty Club | Video', id: req.params.id, caminhoImagem: caminhoImagem });
 });
 
