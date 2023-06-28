1866a6e140b6a0043984f3fed50f6ff7f93721e2
samuel <samuelcostabr5@gmail.com>
2023-06-28 09:36:35 -0300

    video player

--- database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE kitclub;
-USE kitclub;
-CREATE TABLE users(
-    user_id INT NOT NULL PRIMARY KEY,
-    user_name VARCHAR(20) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    user_pass VARCHAR(255) NOT NULL
-);
--- routes/secret.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET Secret page. */
-router.get('/', function(req, res, next){
-    res.render('secret', {title: 'Kitty  Club | Segredo'});
-});
-
-module.exports = router;
