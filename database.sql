cb16d79326cd0ba8041912b5138575bdbead7fd9
almokhan <khanankha@proton.me>
2023-07-03 13:59:09 -0300

    Criado banco de dados para os videos

--- database.sql
+++ database.sql
@@ -4,5 +4,14 @@ CREATE TABLE users(
     user_id INT NOT NULL PRIMARY KEY,
     user_name VARCHAR(20) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
-    user_pass VARCHAR(255) NOT NULL
+    user_pass VARCHAR(255) NOT NULL,
+    user_subs INT(255)
+);
+CREATE TABLE videos(
+    video_id INT NOT NULL PRIMARY KEY,
+    video_name VARCHAR(57) NOT NULL,
+    video_chanel VARCHAR(20) NOT NULL,
+    video_tag VARCHAR(15),
+    video_views INT(255),
+    video_like(255)
 );
